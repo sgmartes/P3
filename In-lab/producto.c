a28751747c59413622b7096e8fae9aa4c7efcc14
Sebastian <jaimepeters2@gmail.com>
2023-03-14 18:01:21 -0600

    Funciones de la HdT agregadas

--- In-lab/producto.c
+++ In-lab/producto.c
@@ -1,16 +1,19 @@
 //producto.c
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX 32
 
 typedef struct{
     int barcode;
-    char *name;
+    char name[MAX];
     float price;
 }Product;
 
 void Product_Ctor(Product* this, int barcode, char* name, float price){
     this->barcode=barcode;
-    this->name=name;
+    strncpy(this->name,name,MAX);
     this->price=price;
 }
 
@@ -18,19 +21,19 @@ void Product_SetBarcode(Product* this, int barcode){
     this->barcode=barcode;
 }
 void Product_SetName(Product* this, char* name){
-    this->name=name;
+    strncpy(this->name,name,MAX);
 }
 void Product_SetPrice(Product* this, float price){
     this->price=price;
 }
 
-int Product_GetBarcode(Product* this){
+const int Product_GetBarcode(const Product* this){
     return this->barcode;
 }
-char* Product_GetName(Product* this){
+const char* Product_GetName(const Product* this){
     return this->name;
 }
-float Product_GetPrice(Product* this){
+const float Product_GetPrice(const Product* this){
     return this->price;
 }
 
@@ -39,18 +42,50 @@ void Product_Imprimir(Product* this){
     printf("Nombre: %s\nPrecio: %.2f\nC. de barras: %d\n",this->name,this->price,this->barcode);
 }
 
-int main(){
-    Product sabritas,leche[3];
-    Product_Ctor(&sabritas,102,"limón y sal",15.4);
-    Product_Imprimir(&sabritas);
+void Product_Assign(Product* this, const Product* other){
+    *this = *other;
+}
 
+bool Product_IsEqual(const Product* this, const Product* other){
+    if (this->barcode==other->barcode && strcmp(this->name,other->name)==0 && this->price==other->price){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool Product_IsEqual_ByBarcode(const Product* this, const Product* other){
+    if (this->barcode==other->barcode){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main(){
+    Product sabritas;
+    Product_Ctor(&sabritas,102,"limón y sal",15.4);
+    Product_SetBarcode(&sabritas,123);
+    float precio_sabritas=Product_GetPrice(&sabritas);
+    Product_SetName(&sabritas,"adobadas");
+
+    Product leche[3];
     for (size_t i=0;i<3;++i){
         Product_Ctor(&leche[i],159,"deslactosada",25.52);
     }
 
+    float precio_leche=0;
+    printf("¿A cuánto está la leche? ");
+    scanf("%f",&precio_leche);
+    Product_SetPrice(&leche[1],precio_leche);
+
+    /*
     Product_SetName(&leche[0],"Entera");
     Product_SetBarcode(&leche[1],199);
     Product_SetPrice(&leche[2],5829.01);
+    */
 
     printf("\nInformación de los productos\n");
     for (size_t i=0;i<3;++i){
@@ -59,4 +94,18 @@ int main(){
         printf("Precio: %.2f\n",Product_GetPrice(&leche[i]));
         printf("C. de barras: %d\n\n",Product_GetBarcode(&leche[i]));
     }
+
+    Product p1;
+    Product_Ctor(&p1,130,"Lubriderm chocolate",90.01);
+    Product p2;
+    Product_Assign(&p2,&p1);
+    
+    //Product_Imprimir(&p2);
+    if (Product_IsEqual(leche,&leche[1])){
+        printf("SON IGUALES\n\n");
+    }
+    else{
+        printf("No son iguales :()");
+    }
+
 }
